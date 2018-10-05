97e46cc3105c1010590d59c5758ff075839dc6c5
mzxrules <mzxrules@gmail.com>
2018-10-05 09:21:35 -0400

    Delete legacy TextArray.py file

--- Patches.py
+++ Patches.py
@@ -9,7 +9,6 @@ import random
 from Hints import buildGossipHints, buildBossRewardHints
 from Utils import local_path, output_path
 from Items import ItemFactory, item_data
-from TextArray import text_array
 
 
 def patch_rom(world, rom):
