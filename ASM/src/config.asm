a3068eecfc398cb493226c9cbbb45be4641c3aca
mzxrules <mzxrules@gmail.com>
2018-12-26 05:16:36 -0500

    Remove oot code

--- ASM/src/config.asm
+++ ASM/src/config.asm
@@ -2,7 +2,7 @@
 ; Settings and tables which the front-end may write
 ;==================================================================================================
 
-; 0x03481000: Item override table:
+; Item override table:
 ;
 ; This table changes the meaning of a given item ID within a given scene. It must be terminated with
 ; four 0x00 bytes (which will happen by default as long as you don't fill the allotted space).
@@ -18,7 +18,7 @@
 ITEM_OVERRIDES:
 .endarea
 
-; 0x03481800: Initial Save Data table:
+; Initial Save Data table:
 ;
 ; This table describes what extra data should be written when a new save file is created. It must be terminated with
 ; four 0x00 bytes (which will happen by default as long as you don't fill the allotted space).
@@ -44,17 +44,5 @@ PLAYER_NAME_ID:
 PLAYER_NAMES:
 .endarea
 
-; 0x03481C00: Special items
+; Special items
 
-LIGHT_ARROW_ITEM:
-.byte 0x5A
-FAIRY_OCARINA_ITEM:
-.byte 0x3B
-FAIRY_ITEMS:
-.byte 0x5D ; Zora's Domain
-.byte 0x5C ; Hyrule Castle
-.byte 0x5E ; Desert Colossus
-.byte 0x51 ; Mountain Summit
-.byte 0x52 ; Crater
-.byte 0x53 ; Ganon's Castle
-.align 4
