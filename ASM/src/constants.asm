40fccd19fc27443b8b4b56610e3625ee63b300d4
mzxrules <mzxrules@gmail.com>
2018-12-26 05:00:49 -0500

    Created every frame hook

--- ASM/src/constants.asm
+++ ASM/src/constants.asm
@@ -4,15 +4,9 @@
 .definelabel G_PAYLOAD_ADDR, (0x80780000 - G_PAYLOAD_SIZE)
 
 ; Pointers to game state
-.definelabel SAVE_CONTEXT,   0x8011A5D0
-.definelabel GLOBAL_CONTEXT, 0x801C84A0
-.definelabel PLAYER_ACTOR,   0x801DAA30
+.definelabel SAVE_CONTEXT,   0x801EF670
+.definelabel GLOBAL_CONTEXT, 0x803E6B20
 .definelabel GET_ITEMTABLE,  0x803A9E7E
 
-; Extended memory map:
-; Loaded code files   0x80400000
-; ASM working memory  0x80480000
-; C working memory    0x80500000
-
-.definelabel DUMMY_ACTOR, 0x80480000
-.definelabel C_HEAP,      0x80500000
+;.definelabel DUMMY_ACTOR, ?
+;.definelabel C_HEAP,      ?
