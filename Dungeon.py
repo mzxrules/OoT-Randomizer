6c67ab7f153513c74f7fde1572df6071d273d46c
Felix Paulusma <felix.paulusma@gmail.com>
2019-02-18 23:53:24 +0100

    Dungeon.py: cleaned

--- Dungeon.py
+++ Dungeon.py
@@ -2,7 +2,7 @@ class Dungeon(object):
 
     def __init__(self, world, name, boss_key, small_keys, dungeon_items):
         def to_array(obj):
-            if obj == None:
+            if obj is None:
                 return []
             if isinstance(obj, list):
                 return obj
@@ -19,37 +19,34 @@ class Dungeon(object):
         for region in world.regions:
             if region.dungeon == self.name:
                 region.dungeon = self
-                self.regions.append(region)                
-
+                self.regions.append(region)
 
     def copy(self, new_world):
         new_boss_key = [item.copy(new_world) for item in self.boss_key]
         new_small_keys = [item.copy(new_world) for item in self.small_keys]
         new_dungeon_items = [item.copy(new_world) for item in self.dungeon_items]
 
-        new_dungeon = Dungeon(new_world, self.name, new_boss_key, new_small_keys, new_dungeon_items)
+        new_dungeon = Dungeon(new_world,
+                              self.name,
+                              new_boss_key,
+                              new_small_keys,
+                              new_dungeon_items)
 
         return new_dungeon
-
 
     @property
     def keys(self):
         return self.small_keys + self.boss_key
 
-
     @property
     def all_items(self):
         return self.dungeon_items + self.keys
 
-
     def is_dungeon_item(self, item):
         return item.name in [dungeon_item.name for dungeon_item in self.all_items]
-
 
     def __str__(self):
         return str(self.__unicode__())
 
-
     def __unicode__(self):
         return '%s' % self.name
-
